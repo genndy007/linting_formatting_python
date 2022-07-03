9c6203d769a2df18a21ceeda868fc089025510ce
genndy007 <kochevgenazp@gmail.com>
2022-07-04 02:43:20 +0300

    Upgrade lint staged script to lint only tracked files

--- lint_staged.sh
+++ lint_staged.sh
@@ -1,9 +1,4 @@
-echo "Running black and mypy"
 for file_path in $(git diff --cached --name-only); do
-  [[ "$file_path" =~ ^.*\.py$ ]] && black --check $file_path && mypy $file_path
+  [[ "$file_path" =~ ^.*\.py$ ]] && black --check $file_path && mypy $file_path && flake8 $file_path && isort --check-only $file_path
 done
-echo "Running flake8"
-flake8
-echo "Running isort check imports"
-isort --check-only .
 exit 0
